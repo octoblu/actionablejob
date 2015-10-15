66c9e011232f9c8d5d4b269dea63de8c039d85ac
sqrt <sqrt@octoblu.com>
2015-10-15 14:59:46 -0700

    Add NewFromJob

--- redisjob.go
+++ redisjob.go
@@ -15,6 +15,11 @@ func New(key string) *RedisJob {
   return &RedisJob{key: key}
 }
 
+// NewFromJob returns a new job based on a Job
+func NewFromJob(job Job) *RedisJob {
+  return &RedisJob{key: job.GetKey()}
+}
+
 // GetKey returns the key to store the information about the job
 func (job *RedisJob) GetKey() string {
   return job.key
--- redisjob_test.go
+++ redisjob_test.go
@@ -7,16 +7,38 @@ import (
 	. "github.com/onsi/gomega"
 )
 
+type DumbJob struct {}
+func (dumbJob *DumbJob) GetKey() string {
+	return "I am dumb"
+}
+
 var _ = Describe("Redisjob", func() {
-	Context("New with a key", func() {
-		var sut redisjob.Job
+	Describe("New", func() {
+		Context("called with a key", func(){
+			var sut redisjob.Job
 
-		BeforeEach(func(){
-			sut = redisjob.New("old-map")
+			BeforeEach(func(){
+				sut = redisjob.New("old-map")
+			})
+
+			It("should set the key", func(){
+				Expect(sut.GetKey()).To(Equal("old-map"))
+			})
 		})
+	})
 
-		It("should set the key", func(){
-			Expect(sut.GetKey()).To(Equal("old-map"))
+	Describe("NewFromJob", func() {
+		Context("called with a job", func(){
+			var sut redisjob.Job
+
+			BeforeEach(func(){
+				job := new(DumbJob)
+				sut = redisjob.NewFromJob(job)
+			})
+
+			It("should set the key", func(){
+				Expect(sut.GetKey()).To(Equal("I am dumb"))
+			})
 		})
 	})
 })
