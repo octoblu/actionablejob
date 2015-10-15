539563fa54497dc6bfe831f77337dff10dcd87d0
sqrt <sqrt@octoblu.com>
2015-10-15 15:32:49 -0700

    Add Claim method

--- actionablejob.go
+++ actionablejob.go
@@ -1,5 +1,11 @@
 package actionablejob
 
+import (
+  "strconv"
+  "time"
+  "github.com/garyburd/redigo/redis"
+)
+
 // Job represents a job in the queue
 type Job interface {
   GetKey() string
@@ -8,6 +14,7 @@ type Job interface {
 // ActionableJob represents a job in the queue that can know if it should have action taken upon it
 type ActionableJob interface {
   GetKey() string
+  Claim() (bool,error)
 }
 
 // ActionableRedisJob implements ActionableJob and stores/retrieves jobs from Redis
@@ -28,4 +35,43 @@ func NewFromJob(job Job) *ActionableRedisJob {
 // GetKey returns the key to store the information about the job
 func (job *ActionableRedisJob) GetKey() string {
   return job.key
+}
+
+// Claim returns true when the caller succesfully claims the job
+func (job *ActionableRedisJob) Claim() (bool,error) {
+  var err error
+  var redisConn redis.Conn
+  var result interface{}
+
+	redisConn, err = redis.Dial("tcp", ":6379")
+
+  if err != nil {
+    return false, err
+  }
+
+  now  := time.Now().Unix()
+  then := now + 1
+  result,err = redisConn.Do("GETSET", "[namespace]-faulty", then)
+
+  nextTick := parseNextTick(result)
+
+	if now < nextTick {
+		return false, nil
+	}
+
+  return true, nil
+}
+
+func parseNextTick(redisResult interface{}) int64 {
+  strNextTick,ok := redisResult.([]uint8)
+  if !ok {
+    return 0
+  }
+
+  nextTick,err := strconv.ParseInt(string(strNextTick), 10, 64)
+  if err != nil {
+    return 0
+  }
+
+  return nextTick
 }
--- actionablejob_test.go
+++ actionablejob_test.go
@@ -1,6 +1,10 @@
 package actionablejob_test
 
 import (
+	"strconv"
+	"time"
+
+	"github.com/garyburd/redigo/redis"
 	"github.com/octoblu/actionablejob"
 
 	. "github.com/onsi/ginkgo"
@@ -13,10 +17,22 @@ func (dumbJob *DumbJob) GetKey() string {
 }
 
 var _ = Describe("ActionableRedisJob", func() {
+	var redisConn redis.Conn
+	var sut actionablejob.ActionableJob
+
+	BeforeEach(func() {
+		var err error
+
+		redisConn, err = redis.Dial("tcp", ":6379")
+		Expect(err).To(BeNil())
+	})
+
+	AfterEach(func(){
+		redisConn.Close()
+	})
+
 	Describe("New", func() {
 		Context("called with a key", func(){
-			var sut actionablejob.ActionableJob
-
 			BeforeEach(func(){
 				sut = actionablejob.New("old-map")
 			})
@@ -29,8 +45,6 @@ var _ = Describe("ActionableRedisJob", func() {
 
 	Describe("NewFromJob", func() {
 		Context("called with a job", func(){
-			var sut actionablejob.ActionableJob
-
 			BeforeEach(func(){
 				job := new(DumbJob)
 				sut = actionablejob.NewFromJob(job)
@@ -38,6 +52,52 @@ var _ = Describe("ActionableRedisJob", func() {
 
 			It("should set the key", func(){
 				Expect(sut.GetKey()).To(Equal("I am dumb"))
+			})
+		})
+	})
+
+	Describe("Claim", func(){
+		BeforeEach(func(){
+			sut = actionablejob.New("faulty")
+		})
+
+		Context("When the job has already run this second", func(){
+			BeforeEach(func(){
+				then := int64(time.Now().Unix() + 1)
+				_,err := redisConn.Do("SET", "[namespace]-faulty", then)
+				Expect(err).To(BeNil())
+			})
+
+			It("should return false", func(){
+				Expect(sut.Claim()).To(BeFalse())
+			})
+		})
+
+		Context("When the job ran in the previous second", func(){
+			var gotClaim bool
+
+			BeforeEach(func(){
+				now := int64(time.Now().Unix())
+				_,err := redisConn.Do("SET", "[namespace]-faulty", now)
+				Expect(err).To(BeNil())
+
+				gotClaim,err = sut.Claim()
+			})
+
+			It("should return false", func(){
+				Expect(gotClaim).To(BeTrue())
+			})
+
+			It("should advance the time", func(){
+				result,err := redisConn.Do("GET", "[namespace]-faulty")
+				Expect(err).To(BeNil())
+
+				nextTickStr := string(result.([]uint8))
+			  nextTick,err := strconv.ParseInt(nextTickStr, 10, 64)
+				Expect(err).To(BeNil())
+
+				then := int64(time.Now().Unix()) + 1
+				Expect(nextTick).To(Equal(then))
 			})
 		})
 	})
